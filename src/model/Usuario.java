852e2ad25ca40b10ecfd89591175a81714dce783
Vinícius <viniciusstorch62@gmail.com>
2025-09-29 19:05:53 -0300

    Classe usuário implementada

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,5 +1,25 @@
 package model;
 
 public class Usuario {
+    private String nome;
+    private String cpf;
+    private String senha;
 
+    public Usuario(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
