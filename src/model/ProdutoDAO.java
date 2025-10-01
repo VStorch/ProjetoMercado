a917f27e4ea03ea8e638fc053389ef138ad0f27f
Vinícius <viniciusstorch62@gmail.com>
2025-10-01 09:08:56 -0300

    Implementação de adicionarProduto em ProdutoDAO

--- src/model/ProdutoDAO.java
+++ src/model/ProdutoDAO.java
@@ -2,6 +2,7 @@ package model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /** Equivale à classe Supermercado, contudo escolhi ProdutoDAO para manter a consistência com o restante do sistema */
 public class ProdutoDAO {
@@ -11,6 +12,20 @@ public class ProdutoDAO {
         String sql = "INSERT INTO produtos (nome, descricao, quantidade, preco) VALUES (?, ?, ?, ?)";
         Connection conexao = null;
         PreparedStatement statement = null;
+
+        try {
+            conexao = BancoDeDados.conectar();
+            statement = conexao.prepareStatement(sql);
+            statement.setString(1, produto.getNome());
+            statement.setString(2, produto.getDescricao());
+            statement.setInt(3, produto.getQuantidade());
+            statement.setDouble(4, produto.getPreco());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            BancoDeDados.desconectar(conexao);
+        }
     }
 
 }
