05164d0b5e31eeed52863eaca6f93477b84bc342
Vinícius <viniciusstorch62@gmail.com>
2025-10-07 23:54:15 -0300

    Ajustes no layout de TelaCompra

--- src/view/TelaCompra.java
+++ src/view/TelaCompra.java
@@ -1,19 +1,22 @@
 package view;
 
+import model.Produto;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.util.List;
 
 public class TelaCompra extends JPanel {
     private JTable tabelaProdutos, tabelaCarrinho;
-    private DefaultTableModel modeloTabela;
+    private DefaultTableModel modeloProdutos, modeloCarrinho;
     private JButton btnAdicionar, btnRemover, btnCarrinho;
 
     public TelaCompra() {
         setLayout(new BorderLayout());
         setPreferredSize(new Dimension(920, 400));
 
-        modeloTabela = new DefaultTableModel(
+        modeloProdutos = new DefaultTableModel(
                 new Object[] {"ID", "Nome", "Descrição", "Quantidade", "Preço"}, 0
         ) {
             public boolean isCellEditable(int row, int column) {
@@ -21,10 +24,18 @@ public class TelaCompra extends JPanel {
             }
         };
 
-        tabelaProdutos = new JTable(modeloTabela);
+        modeloCarrinho = new DefaultTableModel(
+                new Object[] {"ID", "Nome", "Quantidade", "Preço Unitário", "Subtotal"}, 0
+        ) {
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+
+        tabelaProdutos = new JTable(modeloProdutos);
         JScrollPane scrollPane1 = new JScrollPane(tabelaProdutos);
 
-        tabelaCarrinho = new JTable(modeloTabela);
+        tabelaCarrinho = new JTable(modeloProdutos);
         JScrollPane scrollPane2 = new JScrollPane(tabelaCarrinho);
 
         JPanel painelBotoes = new JPanel();
@@ -41,5 +52,24 @@ public class TelaCompra extends JPanel {
         add(scrollPane1, BorderLayout.WEST);
         add(scrollPane2, BorderLayout.EAST);
         add(painelBotoes, BorderLayout.SOUTH);
+    }
+
+    public void atualizarTabela(List<Produto> produtos) {
+        modeloProdutos.setRowCount(0);
+        for (Produto p : produtos) {
+            modeloProdutos.addRow(new Object[]{
+                    p.getId(), p.getNome(), p.getDescricao(), p.getQuantidade(), p.getPreco()
+            });
+        }
+    }
+
+    public JButton getBtnAdicionar() {
+        return btnAdicionar;
+    }
+    public JButton getBtnRemover() {
+        return btnRemover;
+    }
+    public JButton getBtnCarrinho() {
+        return btnCarrinho;
     }
 }
